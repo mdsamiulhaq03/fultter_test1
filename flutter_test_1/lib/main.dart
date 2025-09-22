0483aa9db3adfa4215d664c082d1118edd689d0a
mdsamiulhaq03 <samiultahsin2001@gmail.com>
2025-09-22 14:55:39 +0600

    first commit

---
+++ flutter_test_1/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TheSecondSourceApp());
+}
+
+class TheSecondSourceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Second Source',
+      home: WelcomePage(),
+    );
+  }
+}
+
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  final nameController = TextEditingController();
+  String welcomeText = '';
+
+  void showWelcome() {
+    setState(() {
+      welcomeText = 'Welcome to The Second Source!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('The Second Source'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              elevation: 3,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter your name',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: showWelcome,
+              child: Text('Submit'),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
